0eaf859337041f96fff137e2b674d9684934d0a7
lionel <maid1214@gmail.com>
2021-11-16 14:18:51 +0800

    This is first upDate

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -9,13 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var reverseLabel: UILabel!
     let message = "Hello Git!"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(message)
+        
+        let reversed = reverse(text: "stressed")
+        print(reversed)
+        reverseLabel.text = reversed
+        
     }
 
-
+    func reverse(text: String) -> String {
+        return String(text.reversed())
+    }
 }
 
