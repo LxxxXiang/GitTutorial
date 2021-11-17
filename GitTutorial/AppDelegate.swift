21baf8f745b082ad5f867a1f1d830c498682af07
lionel <maid1214@gmail.com>
2021-11-17 14:09:04 +0800

    version 1.0 UserNotifications (alert)、(sound)、(badge)

--- GitTutorial/AppDelegate.swift
+++ GitTutorial/AppDelegate.swift
@@ -6,31 +6,40 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // 在程式一啟動即詢問使用者是否接受圖文(alert)、聲音(sound)、數字(badge)三種類型的通知
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.badge, .carPlay], completionHandler: { (granted, error) in
+            if granted {
+                print("允許")
+            } else {
+                print("不允許")
+            }
+        })
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -7,23 +7,13 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
-    @IBOutlet weak var reverseLabel: UILabel!
-    let message = "Hello Git!"
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(message)
-        
-        let reversed = reverse(text: "stressed")
-        print(reversed)
-        reverseLabel.text = reversed
-        
-    }
-
-    func reverse(text: String) -> String {
-        return String(text.reversed())
-    }
+ 
+    }    
 }
 
